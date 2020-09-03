510fa2b7a2b86228f51770d01a3b8acf4e9619db
Ivan Cheng <ivan@ivancheng.co.uk>
2020-09-03 17:07:42 +0100

    readme, cleanup

--- src/server/index.js
+++ src/server/index.js
@@ -14,22 +14,8 @@ app.use("/", express.static(path.join(__dirname, "../public")));
 
 // your API calls
 
-app.get("/photos", async (req, res) => {
-  try {
-    console.log(req)
-    let photos = await fetch(
-      `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=${process.env.API_KEY}`
-    ).then((res) => res.json());
-    res.send(photos);
-  } catch (err) {
-    console.log("error:", err);
-  }
-});
-
 app.post("/photos", async (req, res) => {
   try {
-    console.log("Calling API")
-
     let photos = await fetch(
       `https://api.nasa.gov/mars-photos/api/v1/rovers/${req.body.rover.toLowerCase()}/photos?sol=${req.body.sol}&api_key=${process.env.API_KEY}`
     ).then((res) => res.json());
